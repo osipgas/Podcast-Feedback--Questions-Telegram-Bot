7f7940448477c4b1cb1063590db94651aa225f43
Osip Borodkin <115102730+osipgas@users.noreply.github.com>
2024-10-25 17:40:14 +0300

    Update okey_bot.py

--- okey_bot.py
+++ okey_bot.py
@@ -1,9 +1,6 @@
 import telebot
 from telebot import types
-
-API_TOKEN = 'YOUR_API_TOKEN'
-QUESTIONS_CHAT_ID = "YOUR_QUESTIONS_CHAT_ID"
-REVIEWS_CHAT_ID = "YOUR_REVIEWS_CHAT_ID"
+from config import API_TOKEN, QUESTIONS_CHAT_ID, REVIEWS_CHAT_ID
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -18,6 +15,8 @@ start_text = """Я бот подкаста «Что-то на окейном».
 Я создан специально для того, чтобы вы могли написать свой вопрос или отзыв после прослушивания подкаста. 
 
 Я все передам Саше и Насте."""
+
+
 
 bot.remove_webhook()
 
@@ -44,10 +43,10 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'waiting_for_button')
 def handle_button_press(message):
     if message.text == qustion_button:
-        bot.send_message(message.chat.id, "Введите вопрос:")
+        bot.send_message(message.chat.id, "Введите вопрос:\n*задавая вопрос, вы даете разрешение Саше и Насте ответить на него в общем чате без использования вашего имени/ника (анонимно).")
         user_states[message.chat.id] = 'waiting_for_question'
     elif message.text == review_button:
-        bot.send_message(message.chat.id, "Введите отзыв:")
+        bot.send_message(message.chat.id, "Введите отзыв:\n*оставляя отзыв, вы даете согласие на то чтобы Саша и Настя могли поделиться им в своих соцсетях и соцсетях подкаста")
         user_states[message.chat.id] = 'waiting_for_review'
     else:
         bot.send_message(message.chat.id, "Выберите действующую кнопку.")
@@ -62,12 +61,12 @@ def handle_text_input(message):
     if user_states[message.chat.id] == 'waiting_for_question':
         text_to_send = f"Вопрос от пользавателя @{user_name}: \n\n{message.text}"
         bot.send_message(QUESTIONS_CHAT_ID, text_to_send)
-        bot.reply_to(message, "Ваш вопрос принят!")
+        bot.reply_to(message, "Спасибо за вопрос! Мы скоро вам ответим☺️❤️")
         
     elif user_states[message.chat.id] == 'waiting_for_review':
         text_to_send = f"Отзыв от пользавателя @{user_name}: \n\n{message.text}"
         bot.send_message(REVIEWS_CHAT_ID, text_to_send)
-        bot.reply_to(message, "Ваш отзыв принят!")
+        bot.reply_to(message, "Спасибо за отзыв! Нам очень приятно читать ваши сообщения☺️❤️")
     
     markup = create_one_time_keyboard([qustion_button, review_button])
     user_states[message.chat.id] = 'waiting_for_button'
